0083f16fccb57cabda5d870e6c7006741081a053
DavidRothamelNTIJohanneberg <rothamel.david@gmail.com>
2023-06-18 23:50:39 +0200

    Added console log, since i'm having problems with the common one

--- blackjackReactBackend/GameClasses/Game.cs
+++ blackjackReactBackend/GameClasses/Game.cs
@@ -1,7 +1,23 @@
-﻿namespace blackjackReactBackend.GameClasses
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+
+namespace blackjackReactBackend.GameClasses
 {
     public class Game
     {
+        Game()
+        {
+            Game.AllocConsole();//Startar console
+        }
 
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            AllocConsole();
+        }
+        //Gör något med consolen
+        [DllImport("kernel32.dll", SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        static extern bool AllocConsole();
     }
 }
